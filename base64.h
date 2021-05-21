7e11435f1a6fcc187339342a7bcccee058258f2c
Jaap Crezee <jaap@jcz.nl>
2021-05-21 16:30:55 +0200

    make sure base64.c as compiled with C compiler can be used from C++ code

--- base64.h
+++ base64.h
@@ -17,6 +17,10 @@ Maintainer: Sylvain Miermont
 #ifndef _BASE64_H
 #define _BASE64_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* -------------------------------------------------------------------------- */
 /* --- DEPENDANCIES --------------------------------------------------------- */
 
@@ -57,6 +61,10 @@ int bin_to_b64(const uint8_t * in, int size, char * out, int max_len);
 */
 int b64_to_bin(const char * in, int size, uint8_t * out, int max_len);
 
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 /* --- EOF ------------------------------------------------------------------ */
